fccb40b6ae75408932dec1625f9282956c7c36d3
SilentCruzer <radithyakumar7@gmail.com>
2022-09-06 04:22:00 +0530

    Creates top layout for homepage

--- fitness_ui/lib/MainPages/Home.dart
+++ fitness_ui/lib/MainPages/Home.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,8 +13,56 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Container(
-      child: Center(child: Text("Home"))
+      padding: EdgeInsets.only(left:20, right:20,top: 40, bottom: 40),
+      child: Column(
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    width: screenWidth * 0.175,
+                    height: screenWidth * 0.175,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: NetworkImage('https://img.freepik.com/premium-vector/cute-wizard-with-magic-stick-fairytale-avatar-character-cartoon-illustration_357749-1169.jpg'),
+                          fit: BoxFit.fill
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left:20),
+                    child: Column(
+                      children: [
+                        Text("name"),
+                        Text("date"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+
+        Container(
+          width: screenWidth * 0.175,
+          height: screenWidth * 0.175,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              border: Border.all(width: 1, color: Colors.grey.shade300)),
+          child: Icon(
+            Icons.notifications_active_outlined,
+            color: Colors.black,
+          ),
+        ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
