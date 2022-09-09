b4c91ca301158d4f39b532b4df569b4490e0dfc4
SilentCruzer <radithyakumar7@gmail.com>
2022-09-09 10:23:00 +0530

    Implements basic carousel in home

--- fitness_ui/lib/MainPages/components/Home/HomeSlide.dart
+++ fitness_ui/lib/MainPages/components/Home/HomeSlide.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomeSlide extends StatefulWidget {
   const HomeSlide({Key? key}) : super(key: key);
@@ -10,8 +12,129 @@ class HomeSlide extends StatefulWidget {
 class _HomeSlideState extends State<HomeSlide> {
   @override
   Widget build(BuildContext context) {
+    int _currentIndex=0;
+    List cardList=[
+      Item1(),
+      Item2(),
+    ];
+
+    List<T> map<T>(List list, Function handler) {
+      List<T> result = [];
+      for (var i = 0; i < list.length; i++) {
+        result.add(handler(i, list[i]));
+      }
+      return result;
+    }
+
+
+    return CarouselSlider(
+      options: CarouselOptions(
+        height: 200.0,
+        autoPlay: false,
+        initialPage: 0,
+        enableInfiniteScroll: false,
+        autoPlayInterval: Duration(seconds: 3),
+        autoPlayAnimationDuration: Duration(milliseconds: 800),
+        autoPlayCurve: Curves.fastOutSlowIn,
+        pauseAutoPlayOnTouch: true,
+        aspectRatio: 2.0,
+        onPageChanged: (index, reason) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+      items: cardList.map((card){
+        return Builder(
+            builder:(BuildContext context){
+              return Container(
+                height: MediaQuery.of(context).size.height*0.30,
+                width: MediaQuery.of(context).size.width,
+                child: Card(
+                  color: Colors.blueAccent,
+                  child: card,
+                ),
+              );
+            }
+        );
+      }).toList(),
+    );
+  }
+
+}
+
+class Item1 extends StatelessWidget {
+  const Item1({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      child: Text("Carousel"),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.3, 1],
+            colors: [Color(0xffff4000),Color(0xffffcc66),]
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+              "Data",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold
+              )
+          ),
+          Text(
+              "Data",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w600
+              )
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Item2 extends StatelessWidget {
+  const Item2({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.3, 1],
+            colors: [Color(0xff5f2c82), Color(0xff49a09d)]
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+              "Data",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold
+              )
+          ),
+          Text(
+              "Data",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w600
+              )
+          ),
+        ],
+      ),
     );
   }
 }
