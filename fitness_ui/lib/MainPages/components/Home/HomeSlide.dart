0691764c0f9cd7887d92e50285d7e8126f906876
SilentCruzer <radithyakumar7@gmail.com>
2022-09-12 11:10:00 +0530

    Implements new UI for carousel item

--- fitness_ui/lib/MainPages/components/Home/HomeSlide.dart
+++ fitness_ui/lib/MainPages/components/Home/HomeSlide.dart
@@ -1,4 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:fitness_ui/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -18,15 +19,6 @@ class _HomeSlideState extends State<HomeSlide> {
       Item2(),
     ];
 
-    List<T> map<T>(List list, Function handler) {
-      List<T> result = [];
-      for (var i = 0; i < list.length; i++) {
-        result.add(handler(i, list[i]));
-      }
-      return result;
-    }
-
-
     return CarouselSlider(
       options: CarouselOptions(
         padEnds: false,
@@ -44,15 +36,51 @@ class _HomeSlideState extends State<HomeSlide> {
           });
         },
       ),
-      items: cardList.map((card){
+      items: mainSlideData.map((card){
         return Builder(
             builder:(BuildContext context){
               return Container(
                 height: MediaQuery.of(context).size.height*0.30,
                 width: MediaQuery.of(context).size.width,
-                child: Card(
-                  color: Colors.blueAccent,
-                  child: card,
+                padding: EdgeInsets.all(5),
+                child: Container(
+                  padding: EdgeInsets.only(top: 30),
+                  decoration: new BoxDecoration(
+                      color: Colors.orange.shade50,
+                      borderRadius: BorderRadius.all(Radius.circular(20.0))
+                  ),
+                  child: Row(
+                    children: [
+                      Image.asset(card["image"]!),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.only(top:5, bottom: 5, left: 10, right: 10),
+                            decoration: new BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.all(Radius.circular(20.0))
+                            ),
+                            child: Text(card["type"]!, style: TextStyle(
+                                fontSize: 12,
+                              color: Colors.red
+                            ),),
+                          ),
+                          SizedBox(height: 5,),
+                          Container(
+                            width: screenWidth * 0.4,
+                            child: Text(card["title"]!, style: TextStyle(
+                              fontSize: 20,
+                            ),),
+                          ),
+                          Text(card["title"]!, style: TextStyle(
+                              fontSize: 12,
+                            color: Colors.grey
+                          ),)
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               );
             }
--- fitness_ui/lib/constants.dart
+++ fitness_ui/lib/constants.dart
@@ -1,2 +1,17 @@
 double screenWidth = 0;
 double screenHeight = 0;
+
+const mainSlideData = [
+  {
+    "image": "assets/Meditation.png",
+    "type" : "FITNESS",
+    "title" : "Training Meditation 101",
+    "startDate" : "10th Dec",
+  },
+  {
+    "image": "assets/run.png",
+    "type" : "FITNESS",
+    "title" : "Training Sprinting 101",
+    "startDate" : "15th Dec",
+  }
+];
